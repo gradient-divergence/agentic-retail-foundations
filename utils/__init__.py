673fe2d953ecbeb361c945a086b1b6e1ac402990
Fatih Nayebi <f.nayebi@gmail.com>
2025-04-29 06:57:47 -0400

    add package inits to enable imports

---
+++ utils/__init__.py
@@ -0,0 +1,5 @@
+from .openai_utils import safe_chat_completion  # noqa: F401
+from .env import load_project_dotenv  # noqa: F401
+
+# Automatically load project-level .env once utils is imported.
+load_project_dotenv()
